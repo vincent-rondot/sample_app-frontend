553903ad5a59afe1958ca35eb80781bf1bba2d97
vincent-rondot <vincent.rondot@gmail.com>
2019-02-17 21:03:02 +0100

    Fix login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -55,9 +55,14 @@ export class AuthService {
     if (window.location.hash) {
       this.oidcSecurityService.authorizedCallback();
 
+      //Not ideal... 
+      //1) We should get an event, after Auth is done
+      //2) We should use the router of the parent instead of the href...
       window.opener.document.location.href = "./user";
-      window.close()
-
+      setTimeout(function () {
+        window.close();
+      }, 2000);
     }
   }
 }
+
